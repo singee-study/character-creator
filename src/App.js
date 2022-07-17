b4ae7f2f3908548ee1c863b6a2f8cfb024655b27
Singee <git@singee.me>
2022-07-17 13:50:15 +0800

    Exercise 4: Perspective decoration

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import Bg from "./components/Bg";
 
 function App() {
-  return (
-    <>
-      <CharacterEditor />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Bg/>
+            <CharacterEditor/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
